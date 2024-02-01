2f2ec36df11260c9c78ce851cc5fff7263749f6e
Gergo <kapusi.gergo.levente@students.jedlik.eu>
2024-02-01 12:33:16 +0100

    JS Funtion Logic

--- app.js
+++ app.js
@@ -11,3 +11,27 @@ function toggleNav() {
       menuButtonContainer.style.left = "0";
     }
   }
+
+function activeContent() {
+  const contentBox = document.getElementById("content");
+  contentBox.innerHTML = ''
+  contentBox.innerHTML += '<div id="temacim"><h1 class="text-center">Aktív Játékállapot</h1></div>';
+
+  toggleNav()
+}
+
+function nonactiveContent() {
+  const contentBox = document.getElementById("content");
+  contentBox.innerHTML = ''
+  contentBox.innerHTML += '<div id="temacim"><h1 class="text-center">Nem Aktív Játékállapot</h1></div>';
+
+  toggleNav()
+}
+
+function functionsContent() {
+  const contentBox = document.getElementById("content");
+  contentBox.innerHTML = ''
+  contentBox.innerHTML += '<div id="temacim"><h1 class="text-center">Függvények</h1></div>';
+
+  toggleNav()
+}
