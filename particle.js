b80540d705c06ff2a3d5872a3f312baa4004fb64
NovakiTamas <novaki.tamas@students.jedlik.eu>
2024-02-01 13:23:10 +0100

    particlespeedváltosztatása

--- particle.js
+++ particle.js
@@ -49,7 +49,7 @@ particlesJS("particles-js", {
       },
       "move": {
         "enable": true,
-        "speed": 6,
+        "speed": 1,
         "direction": "none",
         "random": false,
         "straight": false,
